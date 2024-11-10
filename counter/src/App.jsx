38ba0c13ce07dbbb1133fe22211735ec77d6418c
dhara-maru <dharamaru406@gmail.com>
2024-11-10 14:01:37 +0530

    counter using useState

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -5,10 +5,40 @@ import './App.css'
 
 function App() {
 
+  let [counter, dharaCounter] = useState(15)
 
+ // let counter = 5;
+
+  const addvalue = ()=>{
+   //console.log(counter);
+    //counter++;
+    if(counter >= 20){
+      counter = 20;
+    }
+    else{
+      dharaCounter(counter+1)
+    }
+    
+  }
+
+  const removevalue = ()=>{
+    if(counter <= 0){
+      counter =0;
+    }
+    else{
+      dharaCounter(counter-1)
+    }
+    
+  }
   return (
     <>
-     
+     <h1>Chai Code React</h1>
+     <h2>counter value: {counter}</h2>
+
+     <button onClick={addvalue}>Increase</button>
+
+     <br /><br />
+     <button onClick={removevalue}>Decrease</button>
     </>
   )
 }
